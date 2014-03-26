06c75358f6b9267d78693b5e49be5dba9d97e19a
foxblock@gmail.com <foxblock@gmail.com@ff6e2948-f240-7f44-3256-3010c5957cbc>
2014-03-26 22:53:28 +0000

    Added licensing information (fucking finally)

--- trunk/Chapter.cpp
+++ trunk/Chapter.cpp
@@ -1,3 +1,27 @@
+/*
+	Greyout - a colourful platformer about love
+
+	Greyout is Copyright (c)2011-2014 Janek Schäfer
+
+	This file is part of Greyout.
+
+    Greyout is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+	Greyout is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Please direct any feedback, questions or comments to
+	Janek Schäfer (foxblock), foxblock_at_gmail_dot_com
+*/
+
 #include "Chapter.h"
 
 #include <fstream>
--- trunk/_main.cpp
+++ trunk/_main.cpp
@@ -1,3 +1,27 @@
+/*
+	Greyout - a colourful platformer about love
+
+	Greyout is Copyright (c)2011-2014 Janek Schäfer
+
+	This file is part of Greyout.
+
+    Greyout is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+	Greyout is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Please direct any feedback, questions or comments to
+	Janek Schäfer (foxblock), foxblock_at_gmail_dot_com
+*/
+
 #include "MyGame.h"
 
 // DONE: Story
@@ -40,8 +64,8 @@
 // DONE: Add debug output for colour under cursor
 // DONE: Colour mapping to surface format after loading from level file
 // DONE: Debug function to advance game logic by single frames
-// TODO: Avoid converting back and forth of colour values in collision functions by saving collision colours in SDL uint (watch for bit depth!)
-// TODO: Add licensing info
+// TODO: Avoid converting back and forth of colour values in collision functions by saving collision colours in SDL uint and replicating GFX::getPixel code (watch for bit depth!)
+// DONE: Add licensing info
 // TODO: Add statistic tracking (deaths/resets per level, time on level, times completed)
 
 // TODO: Level with three levels: black in lower level, one pit with spikes, a few black boxes on upper level, white in second level with white box to form shaft for the boxes
